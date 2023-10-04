204f0d19c611428547fe5c0c6382b4befc7a17af
Ricardo Alves <ricardospalves@gmail.com>
2023-10-04 17:32:54 -0300

    feat: add /posts endpoint of type POST

--- api/src/routes/postsRoute.ts
+++ api/src/routes/postsRoute.ts
@@ -1,10 +1,22 @@
 import { Request, Response, Router } from 'express'
-import { getPosts } from '../service/postsService'
+import { getPosts, savePost } from '../service/postsService'
+import { PostEntity } from '../entities/Post'
 
 const router = Router()
 
 router.get('/posts', async (request: Request, response: Response) => {
   const posts = await getPosts()
+
+  return response.status(200).json(posts)
+})
+
+router.post('/posts', async (request: Request, response: Response) => {
+  const { content, title } = request.body as Omit<
+    PostEntity,
+    'id' | 'createdAt'
+  >
+
+  const posts = await savePost({ content, title })
 
   return response.status(200).json(posts)
 })
