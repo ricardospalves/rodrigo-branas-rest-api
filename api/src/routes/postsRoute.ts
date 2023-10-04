cf22cf0850fbbcb1d06e1a4b3f86311b1e00c289
Ricardo Alves <ricardospalves@gmail.com>
2023-10-04 11:38:37 -0300

    feat: add /posts endpoint

--- api/src/routes/postsRoute.ts
+++ api/src/routes/postsRoute.ts
@@ -1,5 +1,12 @@
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
+import { getPosts } from '../service/postsService'
 
 const router = Router()
+
+router.get('/posts', async (request: Request, response: Response) => {
+  const posts = await getPosts()
+
+  return response.status(200).json(posts)
+})
 
 export { router }
