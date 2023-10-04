66f5ad93bfb787b03da86bd2d2ae9c4ab4b5f5b9
Ricardo Alves <ricardospalves@gmail.com>
2023-10-04 16:48:57 -0300

    feat: add postData to data layer

--- api/src/data/postsData.ts
+++ api/src/data/postsData.ts
@@ -1,5 +1,18 @@
+import { PostEntity } from '../entities/Post'
 import { database } from '../infra/database'
 
-export const getPosts = () => {
+export const getPosts = (): Promise<PostEntity[]> => {
   return database.query('select * from blog.post')
+}
+
+export const savePost = ({
+  content,
+  createdAt,
+  id,
+  title,
+}: PostEntity): Promise<PostEntity> => {
+  return database.one(
+    'insert into blog.post (id, title, content, createdAt) values ($1, $2, $3, $4) returning *',
+    [id, title, content, createdAt],
+  )
 }
