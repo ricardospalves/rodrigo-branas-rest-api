4f116e791037b9caa17db805ac095e4c7944bc14
Ricardo Alves <ricardospalves@gmail.com>
2023-10-04 18:19:55 -0300

    feat: add getPost and updatePost in data layer

--- api/src/data/postsData.ts
+++ api/src/data/postsData.ts
@@ -5,6 +5,10 @@ export const getPosts = (): Promise<PostEntity[]> => {
   return database.query('select * from blog.post')
 }
 
+export const getPost = (id: string): Promise<PostEntity | null> => {
+  return database.oneOrNone('select * from blog.post where id = $1', [id])
+}
+
 export const savePost = ({
   content,
   createdAt,
@@ -14,6 +18,16 @@ export const savePost = ({
   return database.one(
     'insert into blog.post (id, title, content, createdAt) values ($1, $2, $3, $4) returning *',
     [id, title, content, createdAt],
+  )
+}
+
+export const updatePost = (
+  id: string,
+  { content, title }: Omit<PostEntity, 'id' | 'createdAt'>,
+) => {
+  return database.none(
+    'update blog.post set title = $1, content = $2 where id = $3',
+    [title, content, id],
   )
 }
 
